8c648de3f29ca0a10e9a011148befd7fe21c4578
Francisco <franke1615@gmail.com>
2022-04-17 13:28:10 -0700

    first version of capstone page done

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    config.module.rules.push({
+      test: /\.pdf$/i,
+      type: 'asset/source'
+    })
+    return config
+  },
 }
