254aa3155b0e69a8ed6e1c9c9781a93a5f1d92a6
franke1615 <franke1615@gmail.com>
2022-05-29 16:10:49 -0700

    fixing vercel warnings

--- components/projectpanel.js
+++ components/projectpanel.js
@@ -16,12 +16,12 @@ export default function ProjectPanel() {
                             <h4>F1 Calendar App</h4>
                         </Row>
                         <Row style={{marginBottom:5}}>
-                            <a className='d-flex justify-content-center' href="https://github.com/franny1615/F1CalendarApp" target="_blank">
+                            <a className='d-flex justify-content-center' href="https://github.com/franny1615/F1CalendarApp" target="_blank" rel="noreferrer">
                                 <img src="/logos/githubIcon.png" alt="Github" width={30} height={30}/>
                             </a>
                         </Row>
                         <Row className='d-flex justify-content-center' style={{height:'90%'}}>
-                            <Image src={f1AppImg} height='90%' width='370px'/>
+                            <Image src={f1AppImg} height='90%' width='370px' alt="Main F1 Page for my app"/>
                         </Row>
                     </Container>
                 </Col>
@@ -31,12 +31,12 @@ export default function ProjectPanel() {
                             <h4>Expense Management App</h4>
                         </Row>
                         <Row style={{marginBottom:5}}>
-                            <a className='d-flex justify-content-center' href="https://github.com/franny1615/capstone_kotlin" target="_blank">
+                            <a className='d-flex justify-content-center' href="https://github.com/franny1615/capstone_kotlin" target="_blank" rel="noreferrer">
                                 <img src="/logos/githubIcon.png" alt="Github" width={30} height={30}/>
                             </a>
                         </Row>
                         <Row className='d-flex justify-content-center' style={{height:'90%'}}>
-                            <Image src={expenseAppImg} height='90%' width='370px'/>
+                            <Image src={expenseAppImg} height='90%' width='370px' alt="Main Expense App img"/>
                         </Row>
                     </Container>
                 </Col>
