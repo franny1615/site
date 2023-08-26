68ddf05394fffef92c8392f8e963b61b87f1fdc5
frank <franciscofunes5@gmail.com>
2023-08-26 10:05:12 -0700

    moved all images to webp format, simplified main pages and navigation.

--- components/navigationbar.js
+++ components/navigationbar.js
@@ -5,22 +5,11 @@ import Image from 'next/image'
 
 export default function NavigationBar() {
   return (
-    <Navbar bg="light" expand="lg" variant='light'>
-        <Container fluid>
-        <Navbar.Brand className="d-lg-none ml-auto" href="/"><Image src={MyLogo} width={194} height={200}/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto d-flex align-items-center justify-content-center">
-                <Nav.Link href="/education"><h3>Education</h3></Nav.Link>
-                <Nav.Link href="/capstone"><h3>Capstone</h3></Nav.Link>
-                <Nav.Link href="/" className='d-none d-lg-block'>
-                  <Image src={MyLogo} width={194} height={200}/>
-                </Nav.Link>
-                <Nav.Link href="/resume"><h3>Resume</h3></Nav.Link>
-                <Nav.Link href="/contact"><h3>Contact</h3></Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-        </Container>
+    <Navbar className="d-flex justify-content-center align-items-center" bg="dark" expand="lg" variant='light' >
+      <Navbar.Brand href="/">
+        <h2 style={{ color: 'white', textAlign: 'center', margin: 0 }}>Francisco Funes</h2>
+        <p style={{ color: 'white', textAlign: 'center', margin: 0 }}>Software Engineer</p>
+      </Navbar.Brand>
     </Navbar>
   )
 }
